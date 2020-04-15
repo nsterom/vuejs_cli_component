4d517c3ffcfcca39dcb89ae73d58f6d68117b223
mo141933 <lai_iekleong_mac@mail.notes.bank-of-china.com>
2020-04-15 10:55:37 +0800

    create component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Test from './Test.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#test',
+  render: h => h(Test)
 })
